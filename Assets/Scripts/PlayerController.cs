b81f6b9435eeb98c699f67d3ec077a86c2667e24
Ali Kara <56256760+alikaraa@users.noreply.github.com>
2021-12-27 14:59:09 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){  //bosluk tusuna bastiginda ziplasin
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){ 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
